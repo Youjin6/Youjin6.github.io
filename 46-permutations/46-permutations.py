69f53527ab2a7a68bda731675a286354f7f1bf50
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-08-11 17:54:07 -0700

    Time: 84 ms (11.48%), Space: 14.1 MB (58.61%) - LeetHub

--- 46-permutations/46-permutations.py
+++ 46-permutations/46-permutations.py
@@ -1,21 +1,19 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         res = []
-        path = []
+
         n = len(nums)
-        visited = [False for _ in range(len(nums))]
+
         
-        def dfs(u):
-            if u == n:
-                res.append(path[:])
-                return 
+        def dfs(cur):
+            if cur == n:
+                res.append(nums[:])
+                return
             
-            for i in range(n):
-                if not visited[i]:
-                    visited[i] = True
-                    path.append(nums[i])
-                    dfs(u + 1)
-                    visited[i] = False
-                    path.pop()
+            for i in range(cur, n):
+                nums[i], nums[cur] = nums[cur], nums[i]
+                dfs(cur + 1)
+                nums[i], nums[cur] = nums[cur], nums[i]
         dfs(0)
         return res
+                    
