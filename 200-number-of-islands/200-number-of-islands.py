866e9c713fe04e42c15dee4e9157207c67fc749c
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-08-14 09:45:01 -0700

    Time: 507 ms (46.20%), Space: 16.4 MB (80.79%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.py
@@ -0,0 +1,29 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        n = len(grid)
+        m = len(grid[0])
+        dx = [-1, 1, 0, 0]
+        dy = [0, 0, -1, 1]
+        
+        def dfs(x, y):
+            grid[x][y] = "0"
+            for i in range(4):
+                a = x + dx[i]
+                b = y + dy[i]
+                
+                
+                if 0 <= a < n and 0 <= b < m and grid[a][b] == "1":
+                    dfs(a, b)
+        
+        ans = 0
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == '1':
+                    dfs(i, j)
+                    ans += 1
+        return ans
+        
+       
+        
+       
+                
