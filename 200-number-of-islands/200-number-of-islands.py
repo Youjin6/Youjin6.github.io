64f2638e4dfc618744c60894b9d784f52ab718ee
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-08-22 16:12:51 -0700

    Time: 608 ms (23.72%), Space: 18.5 MB (38.48%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,36 +1,48 @@
+class UnionFind:
+    def __init__(self, grid):
+        n, m = len(grid), len(grid[0])
+        self.count = 0
+        self.parent = [None] * (m * n)
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == "1":
+                    self.parent[m * i + j] = m * i + j
+                    self.count += 1
+    
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        px = self.find(x)
+        py = self.find(y)
+        if px != py:
+            self.parent[py] = px
+            self.count -= 1
+    def getCount(self):
+        return self.count
+        
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         n = len(grid)
         m = len(grid[0])
-        dx = [-1, 1, 0, 0]
-        dy = [0, 0, -1, 1]
+        if not n or not m:
+            return 0
+        uf = UnionFind(grid)
         
-        def bfs(x, y):
-            
-            q = deque([(x, y)])
-            while q:
-                row, col = q.popleft()
-                for i in range(4):
-                    a = row + dx[i]
-                    b = col + dy[i]
-                    
-                    if 0 <= a < n and 0 <= b < m and grid[a][b] == "1":
-                        grid[a][b] = "0"
-                        q.append((a, b))
-        
-        ans = 0
         for i in range(n):
             for j in range(m):
-                if grid[i][j] == '1':
+                if grid[i][j] == "1":
                     grid[i][j] = "0"
-                    bfs(i, j)
-                    ans += 1
-        return ans
-        
-       
-"""
-TC: O(nm) | at most it will search each grid once
-SC: O(min(M,N)) | because in worst case where the grid is filled with lands, the size of queue can grow up to min(M,NM,N)
-"""
-       
-                
+                    for x, y in [(i - 1, j),(i + 1, j),(i, j + 1),(i, j - 1)]:
+                        if x in range(n) and y in range(m) and grid[x][y] == "1":
+                            uf.union(m * i + j, m * x + y)
+        return uf.getCount()
+                    
+                    
+                    
+                    
+                    
+                    
+                    
