bebd6c9b848f48ca2409fbcbac75f0f80b9429ae
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-08-22 10:04:08 -0700

    Time: 60 ms (32.80%), Space: 14.2 MB (35.80%) - LeetHub

--- 78-subsets/78-subsets.py
+++ 78-subsets/78-subsets.py
@@ -1,15 +1,19 @@
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans = []
+        res = []
         path = []
+        n = len(nums)
+        
         def dfs(u):
-            if u == len(nums):
-                ans.append(path[:])
+            if u == n:
+                res.append(path[:])
                 return
             
             path.append(nums[u])
             dfs(u + 1)
             path.pop()
             dfs(u + 1)
+        
         dfs(0)
-        return ans
+        return res
+    
