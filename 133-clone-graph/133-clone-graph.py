0aa7af0fa1e8795174be18a66c4688931fe5df47
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-08-23 20:58:41 -0700

    Time: 63 ms (42.96%), Space: 14.4 MB (27.32%) - LeetHub

--- 133-clone-graph/133-clone-graph.py
+++ 133-clone-graph/133-clone-graph.py
@@ -10,20 +10,21 @@ class Solution:
     def cloneGraph(self, node: 'Node') -> 'Node':
         if not node:
             return None
-        
         dic = {}
         
         def dfs(node):
             if node not in dic:
                 dic[node] = Node(node.val)
-                
                 for nei in node.neighbors:
                     if nei not in dic:
                         dfs(nei)
         dfs(node)
         
+        # copy the edges(neighbors)
         for k, v in dic.items():
             for nei in k.neighbors:
                 v.neighbors.append(dic[nei])
         
         return dic[node]
+        
+        
