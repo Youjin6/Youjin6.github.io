249e7b3f3b58cf81da83781992d377c518d7c03b
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-08-25 13:57:04 -0700

    Time: 125 ms (5.94%), Space: 13.9 MB (92.03%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -10,19 +10,18 @@ class Solution:
                 if grid[i][j] == 2:
                     queue.append((i, j ,0))
 
-        def neighbors(r, c) -> (int, int):
-            for nr, nc in ((r - 1, c), (r, c - 1), (r + 1, c), (r, c + 1)):
-                if nr in range(n) and nc in range(m):
-                    yield nr, nc
-
+        
+        dx = [-1, 1, 0, 0]
+        dy = [0, 0, -1, 1]
         d = 0
         while queue:
             r, c, d = queue.popleft()
-            for nr, nc in neighbors(r, c):
-                if grid[nr][nc] == 1:
-                    grid[nr][nc] = 2
-                    queue.append((nr, nc, d + 1))
-
+            for i in range(4):
+                a = r + dx[i]
+                b = c + dy[i]
+                if a in range(n) and b in range(m) and grid[a][b] == 1:
+                    grid[a][b] = 2
+                    queue.append((a,b,d+ 1))
         if any(1 in row for row in grid):
             return -1
         return d
