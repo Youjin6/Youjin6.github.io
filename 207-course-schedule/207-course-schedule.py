3e1da771403d75aa855b40c306751203455fe833
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-08-26 13:16:02 -0700

    Time: 200 ms (20.39%), Space: 17.5 MB (9.80%) - LeetHub

--- 207-course-schedule/207-course-schedule.py
+++ 207-course-schedule/207-course-schedule.py
@@ -1,35 +1,29 @@
 class Solution:
-    """
-    status: 0. NOT VISITED 1. WIP  2.DONE 
-    """
     def canFinish(self, n: int, prerequisites: List[List[int]]) -> bool:
-        dic = collections.defaultdict(list)
-        visited = [0] * n
+        dic = defaultdict(list)
         res = []
         valid = True
+        visited = [0] * n
         
         for [course, pre] in prerequisites:
-            dic[course].append(pre)
+            dic[pre].append(course)
             
-        
-        def dfs(u):
+        def dfs(u: int):
             nonlocal valid
             visited[u] = 1
             for i in dic[u]:
                 if visited[i] == 0:
                     dfs(i)
-                    if not valid:
-                        return
                 elif visited[i] == 1:
                     valid = False
                     return
             visited[u] = 2
             res.append(u)
-            
         
         for i in range(n):
-            if valid and not visited[i]:
+            if valid and visited[i] == 0:
                 dfs(i)
-                
+                if not valid:
+                    return False
         
         return valid
