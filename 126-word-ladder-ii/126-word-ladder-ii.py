03414c643f8abb827021588269de251c38fcd25b
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-08-28 23:27:37 -0700

    Time: 135 ms (45.84%), Space: 14.3 MB (91.09%) - LeetHub

--- 126-word-ladder-ii/126-word-ladder-ii.py
+++ 126-word-ladder-ii/126-word-ladder-ii.py
@@ -1,45 +1,48 @@
 class Solution:
     def findLadders(self, beginWord: str, endWord: str, wordList: List[str]) -> List[List[str]]:
-        dist = defaultdict(lambda: 0)
-        # h = set(wordList)
-        dist[beginWord] = 1
-        
-        queue = deque([beginWord])
-        def get_next_word(word):
-            next_words = []
-            for i in range(len(word)):
-                for c in range(ord('a'), ord('z') + 1):
-                    new_word = list(word)
-                    new_word[i] = chr(c)
-                    new_word = ''.join(new_word)
-                    next_words.append(new_word)
-            return next_words
-        
-        while queue:
-            t = queue.popleft()
-            for next_word in get_next_word(t):
-                if next_word in wordList and dist[next_word] == 0:
-                    dist[next_word] = dist[t] + 1
-                    queue.append(next_word)
-        
-        res = []
-        path = [endWord]
-        
-        
-        
-        def dfs(t):
-            if t == beginWord:
-                res.append(path[::-1])
-                
-            else:
-                for next_word in get_next_word(t):    
-                        
-                    if dist[next_word] + 1 == dist[t]:
-                        path.append(next_word)
-                        dfs(next_word)
-                        path.pop()
-        if endWord not in dist:
+        word_bag = set(wordList)
+        if endWord not in word_bag:
             return []
-        dfs(endWord)
-        return res
-                
+        
+        word_bag.add(beginWord)
+        
+        distance = self.bfs(endWord, word_bag)
+        
+        results = []
+        self.dfs(beginWord, endWord, word_bag, distance, [beginWord], results)
+        
+        return results
+    
+    def bfs(self, begin_word, word_bag):
+        queue = [begin_word]
+        distance = {}
+        distance[begin_word] = 0
+        while queue:
+            size = len(queue)
+            for _ in range(size):
+                curr_word = queue.pop(0)
+                for next_word in self.get_next_words(curr_word, word_bag):
+                    if next_word not in distance:
+                        distance[next_word] = distance[curr_word] + 1
+                        queue.append(next_word)
+        return distance
+    
+    def dfs(self, curr_word, target, word_bag, distance, path, results):
+        if curr_word == target:
+            results.append(list(path))
+            return
+        for next_word in self.get_next_words(curr_word, word_bag):
+            if distance[next_word] != distance[curr_word] - 1:
+                continue
+            path.append(next_word)
+            self.dfs(next_word, target, word_bag, distance, path, results)
+            path.pop()
+    
+    def get_next_words(self, curr_word, word_bag):
+        next_words = []
+        for i in range(len(curr_word)):
+            for c in list(string.ascii_lowercase):
+                next_word = curr_word[:i] + c + curr_word[i + 1:]
+                if next_word != curr_word and next_word in word_bag:
+                    next_words.append(next_word)
+        return next_words
