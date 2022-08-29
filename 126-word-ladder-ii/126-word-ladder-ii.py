f48368cd6e4c577f7e5e4e584becc1949962b1a5
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-08-28 21:58:28 -0700

    Time: 1943 ms (7.59%), Space: 16.8 MB (9.17%) - LeetHub

--- 126-word-ladder-ii/126-word-ladder-ii.py
+++ 126-word-ladder-ii/126-word-ladder-ii.py
@@ -5,40 +5,39 @@ class Solution:
         dist[beginWord] = 1
         
         queue = deque([beginWord])
+        def get_next_word(word):
+            next_words = []
+            for i in range(len(word)):
+                for c in range(ord('a'), ord('z') + 1):
+                    new_word = list(word)
+                    new_word[i] = chr(c)
+                    new_word = ''.join(new_word)
+                    next_words.append(new_word)
+            return next_words
         
         while queue:
             t = queue.popleft()
-            
-            for i in range(len(t)):
-                for c in range(ord('a'), ord('z') + 1):
-                    new_word = list(t)
-                    new_word[i] = chr(c)
-                    new_word = ''.join(new_word)
-                    
-                    if new_word in wordList and dist[new_word] == 0:
-                        dist[new_word] = dist[t] + 1
-                        # if new_word == endWord:
-                        #     break
-                        queue.append(new_word)
+            for next_word in get_next_word(t):
+                if next_word in wordList and dist[next_word] == 0:
+                    dist[next_word] = dist[t] + 1
+                    queue.append(next_word)
         
         res = []
         path = [endWord]
+        
+        
         
         def dfs(t):
             if t == beginWord:
                 res.append(path[::-1])
                 
             else:
-                for i in range(len(t)):
-                    for c in range(ord('a'), ord('z') + 1):
-                        new_word = list(t)
-                        new_word[i] = chr(c)
-                        new_word = ''.join(new_word)     
+                for next_word in get_next_word(t):    
                         
-                        if dist[new_word] + 1 == dist[t]:
-                            path.append(new_word)
-                            dfs(new_word)
-                            path.pop()
+                    if dist[next_word] + 1 == dist[t]:
+                        path.append(next_word)
+                        dfs(next_word)
+                        path.pop()
         dfs(endWord)
         return res
                 
