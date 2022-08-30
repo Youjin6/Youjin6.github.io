280b3ff457c650ff9044ce34e21348452c7aaaac
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-08-30 10:57:28 -0700

    Time: 53 ms (30.93%), Space: 13.8 MB (57.02%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -2,10 +2,12 @@ class Solution:
     def climbStairs(self, n: int) -> int:
         a = 1
         b = 1
-        n -= 1
-        while n:
+        for i in range(n - 1):
             c = a + b
             a = b
             b = c
-            n -= 1
         return b
+        
+        
+    
+
