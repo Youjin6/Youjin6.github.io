320ddec9cd013195e76c1a7d091f07728a810e2d
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-08-30 10:53:07 -0700

    Time: 60 ms (14.33%), Space: 13.9 MB (11.86%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,16 +1,11 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        dic = [None] * (n + 1)
-        dic[0] = 1
-        
-        def process(rest):
-            if rest < 0: return 0
-            elif dic[rest]:
-                return dic[rest]
-
-            dic[rest] = process(rest - 1) + process(rest - 2)
-            return dic[rest]
-        
-        return process(n)
-    
-      
+        a = 1
+        b = 1
+        n -= 1
+        while n:
+            c = a + b
+            a = b
+            b = c
+            n -= 1
+        return b
