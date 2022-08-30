b82c7e7f6eb695d5899067912e5ed1cf9a24cf74
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-08-30 10:42:24 -0700

    Time: 67 ms (5.24%), Space: 13.9 MB (57.02%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.py
+++ 70-climbing-stairs/70-climbing-stairs.py
@@ -1,12 +1,15 @@
 class Solution:
     def climbStairs(self, n: int) -> int:
-        @lru_cache
+        dic = [None] * (n + 1)
+        dic[0] = 1
+        
         def process(rest):
-            if rest == 0:
-                return 1
-            elif rest < 0:
-                return 0
-            return process(rest - 1) + process(rest - 2)
+            if rest < 0: return 0
+            elif dic[rest]:
+                return dic[rest]
+
+            dic[rest] = process(rest - 1) + process(rest - 2)
+            return dic[rest]
         
         return process(n)
     
