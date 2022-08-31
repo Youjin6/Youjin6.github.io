ca5928252082956cbb674201e07d0b742a9a80fb
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-08-31 15:54:27 -0700

    Time: 3916 ms (19.07%), Space: 14 MB (29.05%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,16 +1,16 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        n = len(s)
+        if not s:
+            return 0
         res = ''
-        for i in range(n):
+        n = len(s)
+        for i in range(len(s)):
             l, r = i - 1, i + 1
             while l in range(n) and r in range(n) and s[l] == s[r]:
                 l -= 1
                 r += 1
             if len(res) < r - l - 1:
                 res = s[l + 1: r]
-                
-                
             
             l, r = i, i + 1
             while l in range(n) and r in range(n) and s[l] == s[r]:
@@ -19,3 +19,4 @@ class Solution:
             if len(res) < r - l - 1:
                 res = s[l + 1: r]
         return res
+            
