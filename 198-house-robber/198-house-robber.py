3001acb938e80d4dbad01a6c0bbba03eca79fa2f
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-08-31 13:02:07 -0700

    Time: 63 ms (16.48%), Space: 13.9 MB (65.50%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,11 +1,17 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        f = [0] * (n + 1)
-        g = [0] * (n + 1)
+        if n == 1:
+            return nums[0]
         
-        for i in range(1, n + 1):
-            f[i] = nums[i - 1] + g[i - 1]
-            g[i] = max(f[i - 1], g[i - 1])
+        f = [[0 for _ in range(2)] for _ in range(n)]
+        f[0][0] = 0
+        f[0][1] = nums[0]
         
-        return max(f[n], g[n])
+        for i in range(1, n):
+            f[i][0] = max(f[i - 1][0], f[i - 1][1])
+            f[i][1] = nums[i] + f[i - 1][0]
+            
+        
+        return max(f[n - 1][0], f[n - 1][1])
+        
