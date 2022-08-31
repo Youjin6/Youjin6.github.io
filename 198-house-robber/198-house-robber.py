bb1dc819c01819e44dab8d4d3e012d64a06d04e9
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-08-31 12:50:35 -0700

    Time: 74 ms (5.61%), Space: 13.8 MB (97.21%) - LeetHub

--- 198-house-robber/198-house-robber.py
+++ 198-house-robber/198-house-robber.py
@@ -1,13 +1,11 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
-        
-        f = [0] * (n + 1) # include i
-        g = [0] * (n + 1) # not include i 
+        f = [0] * (n + 1)
+        g = [0] * (n + 1)
         
         for i in range(1, n + 1):
             f[i] = nums[i - 1] + g[i - 1]
-            g[i] = max(g[i - 1], f[i - 1])
+            g[i] = max(f[i - 1], g[i - 1])
         
         return max(f[n], g[n])
-            
