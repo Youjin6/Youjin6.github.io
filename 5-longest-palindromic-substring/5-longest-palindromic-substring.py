6d04bcb94bdf540deedeed5959c3df644e8451a6
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-08-31 16:06:24 -0700

    Time: 1820 ms (35.03%), Space: 13.7 MB (99.42%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -1,37 +1,25 @@
 class Solution:
+    def init(self, s):
+        res = '#'
+        for i in s:
+            res += i
+            res += '#'
+        return res
+    
     def longestPalindrome(self, s: str) -> str:
-        n = len(s)
-        if n < 2:
-            return s
-        
-        max_len = 1
-        begin = 0
-        # dp[i][j] 表示 s[i..j] 是否是回文串
-        dp = [[False] * n for _ in range(n)]
-        for i in range(n):
-            dp[i][i] = True
-        
-        # 递推开始
-        # 先枚举子串长度
-        for L in range(2, n + 1):
-            # 枚举左边界，左边界的上限设置可以宽松一些
-            for i in range(n):
-                # 由 L 和 i 可以确定右边界，即 j - i + 1 = L 得
-                j = L + i - 1
-                # 如果右边界越界，就可以退出当前循环
-                if j >= n:
-                    break
-                    
-                if s[i] != s[j]:
-                    dp[i][j] = False 
-                else:
-                    if j - i < 3:
-                        dp[i][j] = True
-                    else:
-                        dp[i][j] = dp[i + 1][j - 1]
-                
-                # 只要 dp[i][L] == true 成立，就表示子串 s[i..L] 是回文，此时记录回文长度和起始位置
-                if dp[i][j] and j - i + 1 > max_len:
-                    max_len = j - i + 1
-                    begin = i
-        return s[begin:begin + max_len]
+        string = self.init(s)    
+
+        res = ''
+        for i in range(len(string)):
+            l = i - 1
+            r = i + 1
+            while l >= 0 and r < len(string) and string[l] == string[r]:
+                l -= 1
+                r += 1
+            if len(res) < r - l - 1:
+                res = string[l + 1: r]
+        print(res)
+        a = res.split('#')
+        a = ''.join(a)
+        return a
+    
