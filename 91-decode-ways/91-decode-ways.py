cf35e04611d0531b1951598ae3b946d7ddc9d4a0
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-09-02 10:44:43 -0700

    Time: 53 ms (45.53%), Space: 14.3 MB (10.44%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,20 +1,16 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        if not s:
-            return 0
-        n = len(s)
-        dp = [0] * (n + 1)
+        @lru_cache
+        def dfs(cur):
+            if cur == len(s):
+                return 1
+            if s[cur] == '0':
+                return 0
+            
+            # other case
+            res = dfs(cur + 1)
+            if cur + 1 < len(s) and int(s[cur: cur + 2]) in range(10, 27):
+                res += dfs(cur + 2)
+            return res
         
-        dp[n] = 1
-
-        
-        for i in reversed(range(n)):
-            if s[i] != '0':
-            
-                res = dp[i + 1]
-                if i + 1 < len(s) and int(s[i: i + 2]) in range(10, 27):
-                    res += dp[i + 2]
-            
-                dp[i] = res
-            
-        return dp[0]
+        return dfs(0)
