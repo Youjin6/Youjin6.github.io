e7b359442f819fd603e04f159af9e3b9ec9f44bb
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-09-02 14:51:59 -0700

    Time: 1399 ms (91.78%), Space: 14.2 MB (61.85%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -3,9 +3,11 @@ class Solution:
         f = [1e8] * (amount + 1)
         f[0] = 0
         
-        for v in coins:
-            for j in range(v, amount + 1):
-                f[j] = min(f[j], f[j - v] + 1)
+        for i in coins:
+            for j in range(i, amount + 1):
+                f[j] = min(f[j], f[j - i] + 1)
+        
         if f[amount] == 1e8:
             return -1
-        return f[amount]
+        
+        return f[-1]
