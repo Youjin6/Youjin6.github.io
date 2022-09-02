7ffbf419a170fe7a128d001b64be7c04f09b5ab3
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-09-01 18:21:42 -0700

    Time: 2296 ms (5.08%), Space: 13.7 MB (98.78%) - LeetHub

--- 91-decode-ways/91-decode-ways.py
+++ 91-decode-ways/91-decode-ways.py
@@ -1,6 +1,8 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
-        @lru_cache
+        memo = [None] * (len(s) + 1)
+        
+        
         def process(cur):
             # base case
             if cur == len(s):
@@ -8,7 +10,8 @@ class Solution:
             # condition 
             if s[cur] == '0':
                 return 0
-            
+            if memo[cur]:
+                return memo[cur]
             # other case: single digit
             
             res = process(cur + 1)
@@ -16,6 +19,7 @@ class Solution:
             # other case: double digit
             if cur + 1 < len(s) and int(s[cur: cur + 2]) in range(1, 27):
                 res += process(cur + 2)
+            memo[cur] = res
             return res
         
         return process(0)
