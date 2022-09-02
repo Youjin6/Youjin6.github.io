da66f8f30ef95fec537dd307bdee3410d469c0c0
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-09-02 15:31:00 -0700

    Time: 139 ms (98.04%), Space: 13.9 MB (99.15%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.py
+++ 518-coin-change-2/518-coin-change-2.py
@@ -1,27 +1,10 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-#         @lru_cache
-#         def dfs(cur, remain):
-#             if cur == len(coins):
-#                 return 1 if remain == 0 else 0 
-#             i = 0
-#             ans = 0
-#             while i * coins[cur] <= remain:
-#                 ans += dfs(cur + 1, remain - i * coins[cur])
-#                 i += 1
-            
-#             return ans
+        f = [0] * (amount + 1)
+        f[0] = 1
         
+        for i in coins:
+            for j in range(i, amount + 1):
+                f[j] += f[j - i]
         
-        n = len(coins)
-        dp = [[0] * (amount + 1) for _ in range(n + 1)]
-        
-        dp[n][0] = 1
-        
-        for i in reversed(range(n)):
-            for j in range(amount + 1):
-                dp[i][j] = dp[i + 1][j]
-                if j - coins[i] >= 0:
-                    dp[i][j] += dp[i][j - coins[i]]
-        
-        return dp[0][amount]
+        return f[-1]
