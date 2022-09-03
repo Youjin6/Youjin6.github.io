b13c71bd0a27964d6422623282adff8e27e800e5
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-09-03 13:19:25 -0700

    Time: 6689 ms (5.01%), Space: 16.2 MB (26.77%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -11,36 +11,7 @@ class Solution:
                 if j >= coins[i - 1]:
                     f[i][j] = min(f[i][j], f[i][j - coins[i - 1]] + 1) 
                     
-        if f[-1][-1] == 1e8:
+        if f[n][amount] == 1e8:
             return -1
-        return f[-1][-1]
+        return f[n][amount]
     
-    
-"""
-class Solution {
-    public int coinChange(int[] coins, int amount) {
-
-        int l = coins.length;
-        var dp = new int[l + 1][amount + 1];
-
-        // 将第一行的所有元素设置为一个较大值，为了后续求Min。
-        Arrays.fill(dp[0], amount + 1);
-
-        for (int i = 1; i <= l; i++){
-            for (int j = 1; j <= amount; j++){
-                // 选择了第i件物品，并求在金额j范围内满足要求的最小数量。
-                if (j - coins[i - 1] >= 0){
-                    dp[i][j] = Math.min(dp[i - 1][j], dp[i][j - coins[i - 1]] + 1);
-                } else {
-                // 不选第i件物品。
-                    dp[i][j] = dp[i - 1][j];
-                }
-            }
-        }
-        // eg. coins = [2], amount = 3
-        return dp[l][amount] > amount ? -1 : dp[l][amount];
-    }
-}
-
-
-"""
