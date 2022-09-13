dbecb07e98243bd04961dbfeaf65d48e87cd912f
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-09-13 11:24:29 -0700

    Time: 1574 ms (9.57%), Space: 19.3 MB (23.17%) - LeetHub

--- 134-gas-station/134-gas-station.py
+++ 134-gas-station/134-gas-station.py
@@ -1,14 +1,15 @@
 class Solution:
     def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
-        if sum(gas) < sum(cost):
-            return -1
-        
-        total = 0
-        res = 0
-        for i in range(len(gas)):
-            total += (gas[i] - cost[i])
-            if total < 0:
-                total = 0
-                res = i + 1
-        
-        return res
+        n=len(gas)
+        i=0
+        while i<n:#枚举起点
+            left=0
+            for j in range(0,n+1): # 检查能不能从i走j步
+                k=(i+j)%n
+                left+=gas[k]-cost[k]
+                if left<0:
+                    break
+            if j==n: # 满足条件
+                return i
+            i=i+j+1 # 如果从i到不了起点,只能走j步，说明 i~j之间都不可能走通
+        return -1
