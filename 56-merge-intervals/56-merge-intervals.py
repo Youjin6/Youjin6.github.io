67b6213d26247e1eb8096b3ab9868b2c0562895f
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-09-14 16:21:41 -0700

    Time: 156 ms (91.21%), Space: 18.9 MB (26.14%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.py
+++ 56-merge-intervals/56-merge-intervals.py
@@ -1,14 +1,15 @@
 class Solution:
     def merge(self, intervals: List[List[int]]) -> List[List[int]]:
         intervals.sort()
-        print(intervals)
-        res = []
+        
         start = intervals[0][0]
         end = intervals[0][1]
+        res = []
         
         for a, b in intervals:
             if a <= end:
                 end = max(b, end)
+            
             else:
                 res.append([start, end])
                 start = a
@@ -16,3 +17,7 @@ class Solution:
         
         res.append([start, end])
         return res
+
+                
+                
+                
