b90ebb52c49b59636c6544f830ab1ce0d6fb60a5
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-09-16 12:57:54 -0700

    Time: 272 ms (23.05%), Space: 14 MB (8.32%) - LeetHub

--- 43-multiply-strings/43-multiply-strings.py
+++ 43-multiply-strings/43-multiply-strings.py
@@ -1,3 +1,31 @@
 class Solution:
     def multiply(self, num1: str, num2: str) -> str:
-        return str(int(num1) * int(num2))
+        n = len(num1)
+        m = len(num2)
+        res = [0] * (n + m - 1)
+        ans = []
+        for j in reversed(range(m)):
+            for i in reversed(range(n)):
+                k = i + j
+                res[k] += int(num2[j]) * int(num1[i])
+        print(res)
+        carry = 0
+        for i in reversed(res):
+            carry += i
+            ans.insert(0, carry % 10)
+            carry //= 10
+        
+        if carry:
+            ans.insert(0, carry)
+        
+
+        while ans and ans[0] == 0:
+            ans.pop(0)
+        
+        
+        if not ans:
+            return '0'
+        return ''.join(map(str, ans))
+            
+        
+                        
