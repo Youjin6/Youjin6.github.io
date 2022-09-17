47d3ce7d7957d7ced2b69670dc4ffa367880730f
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-09-17 12:19:13 -0700

    Time: 57 ms (50.18%), Space: 13.9 MB (49.80%) - LeetHub

---
+++ 14-longest-common-prefix/14-longest-common-prefix.py
@@ -0,0 +1,10 @@
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ''
+        for i in zip(*strs):
+            if len(set(i)) != 1:
+                return res
+            else:
+                res += i[0]
+        
+        return res
