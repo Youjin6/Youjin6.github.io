a74ebb07a3300b9678fd336ad4dc24635f4cb761
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-09-22 19:31:57 -0700

    Time: 475 ms (58.58%), Space: 16.5 MB (64.02%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,52 +1,24 @@
-class UnionFind:
-    def __init__(self, grid):
-        n, m = len(grid), len(grid[0])
-        self.count = 0
-        self.parent = [None] * (m * n)
-        for i in range(n):
-            for j in range(m):
-                if grid[i][j] == "1":
-                    self.parent[m * i + j] = m * i + j
-                    self.count += 1
-    
-    def find(self, x):
-        if self.parent[x] != x:
-            self.parent[x] = self.find(self.parent[x])
-        return self.parent[x]
-    
-    def union(self, x, y):
-        px = self.find(x)
-        py = self.find(y)
-        if px != py:
-            self.parent[py] = px
-            self.count -= 1
-    def getCount(self):
-        return self.count
-        
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
         n = len(grid)
         m = len(grid[0])
-        if not n or not m:
-            return 0
-        uf = UnionFind(grid)
         dx = [-1, 1, 0, 0]
         dy = [0, 0, -1, 1]
+        
+        def dfs(x, y):
+            grid[x][y] = '0'
+            for i in range(4):
+                a = x + dx[i]
+                b = y + dy[i]
+                
+                if a in range(n) and b in range(m) and grid[a][b] == '1':
+                    dfs(a, b)
+        ans = 0
         for i in range(n):
             for j in range(m):
-                if grid[i][j] == "1":
-                    grid[i][j] = "0"
-                    for k in range(4):
-                        x = i + dx[k]
-                        y = j + dy[k]
-                    
-                        if x in range(n) and y in range(m) and grid[x][y] == "1":
-                            uf.union(m * i + j, m * x + y)
-        return uf.getCount()
-                    
-                    
-                    
-                    
-                    
-                    
-                    
+                if grid[i][j] == '1':
+                    dfs(i, j)
+                    ans += 1
+        
+        return ans
+            
