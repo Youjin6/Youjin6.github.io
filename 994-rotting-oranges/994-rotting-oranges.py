f88f652b4634105b9b5cb42be5159c5322f24620
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-09-23 15:29:45 -0700

    Time: 114 ms (20.30%), Space: 14 MB (48.13%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -7,7 +7,7 @@ class Solution:
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == 2:
-                    queue.append([i, j, 0])
+                    queue.append((i, j, 0))
         
         d = 0
         dx = [-1, 1, 0, 0]
@@ -20,7 +20,7 @@ class Solution:
                 b = y + dy[i]
                 if a in range(n) and b in range(m) and grid[a][b] == 1:
                     grid[a][b] = 2
-                    queue.append([a, b, d + 1])
+                    queue.append((a, b, d + 1))
         
         for i in range(n):
             for j in range(m):
