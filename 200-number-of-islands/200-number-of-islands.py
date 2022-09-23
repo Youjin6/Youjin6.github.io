b7596b1507813eefae1b2f1922b3e16ca6acd2ed
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-09-22 19:37:58 -0700

    Time: 401 ms (75.27%), Space: 16.3 MB (91.29%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -5,20 +5,24 @@ class Solution:
         dx = [-1, 1, 0, 0]
         dy = [0, 0, -1, 1]
         
-        def dfs(x, y):
-            grid[x][y] = '0'
-            for i in range(4):
-                a = x + dx[i]
-                b = y + dy[i]
-                
-                if a in range(n) and b in range(m) and grid[a][b] == '1':
-                    dfs(a, b)
+        def bfs(x, y):
+            
+            q = deque([(x, y)])
+            while q:
+                row, col = q.popleft()
+                for i in range(4):
+                    a = row + dx[i]
+                    b = col + dy[i]
+                    
+                    if 0 <= a < n and 0 <= b < m and grid[a][b] == "1":
+                        grid[a][b] = "0"
+                        q.append((a, b))
+        
         ans = 0
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == '1':
-                    dfs(i, j)
+                    grid[i][j] = "0"
+                    bfs(i, j)
                     ans += 1
-        
         return ans
-            
