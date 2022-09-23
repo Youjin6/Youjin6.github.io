2c8efe44b7a5fe55c0eec852f50648ff46733015
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-09-23 15:39:43 -0700

    Time: 819 ms (12.98%), Space: 17 MB (66.53%) - LeetHub

--- 286-walls-and-gates/286-walls-and-gates.py
+++ 286-walls-and-gates/286-walls-and-gates.py
@@ -4,32 +4,26 @@ class Solution:
         Do not return anything, modify rooms in-place instead.
         """
         n, m = len(rooms), len(rooms[0])
+        queue = deque([])
+
         dx = [-1, 1, 0, 0]
-        dy = [0, 0, -1, 1]
-        INF = 2147483647
-        q = deque([])
+        dy = [0, 0, 1, -1]
+        
+        # add all the door the our queue
         for i in range(n):
             for j in range(m):
                 if rooms[i][j] == 0:
-                    q.append((i, j, 0))
+                    queue.append((i, j, 0))
         
+        # add grid to queue while queue is not empty
         d = 0
-        while q:
-            x, y, d = q.popleft()
-            
+        while queue:
+            x, y, d = queue.popleft()
             for i in range(4):
                 a = x + dx[i]
                 b = y + dy[i]
                 if a in range(n) and b in range(m):
                     if rooms[a][b] > d + 1:
                         rooms[a][b] = d + 1
-                        q.append((a, b, d + 1))
-        
-        
-                        
-                        
-                        
-                        
-                    
-        
+                        queue.append((a, b, d + 1))
                     
