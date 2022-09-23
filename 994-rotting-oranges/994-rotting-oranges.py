a7961556fca450d00de23baaee67b1146b053e16
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-09-23 15:29:26 -0700

    Time: 53 ms (94.50%), Space: 13.9 MB (91.48%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.py
+++ 994-rotting-oranges/994-rotting-oranges.py
@@ -1,28 +1,31 @@
 class Solution:
     def orangesRotting(self, grid: List[List[int]]) -> int:
         n, m = len(grid), len(grid[0])
-        dx = [-1, 1, 0, 0]
-        dy = [0, 0, -1, 1]
-        queue = collections.deque()
+        
+        queue = deque([])
         
         for i in range(n):
             for j in range(m):
                 if grid[i][j] == 2:
-                    queue.append((i, j, 0))
-                    
+                    queue.append([i, j, 0])
+        
         d = 0
+        dx = [-1, 1, 0, 0]
+        dy = [0, 0, -1, 1]
+        
         while queue:
             x, y, d = queue.popleft()
-            
             for i in range(4):
                 a = x + dx[i]
                 b = y + dy[i]
                 if a in range(n) and b in range(m) and grid[a][b] == 1:
-                    queue.append((a, b, d + 1))
                     grid[a][b] = 2
+                    queue.append([a, b, d + 1])
         
-        if any(1 in row for row in grid):
-            return -1
+        for i in range(n):
+            for j in range(m):
+                if grid[i][j] == 1:
+                    return -1
+        
         return d
                     
-                
