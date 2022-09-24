6666968c321fdb70443e6a73ae3e3d0d35dbc5bb
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-09-23 21:34:16 -0700

    Time: 172 ms (6.62%), Space: 14.3 MB (91.95%) - LeetHub

--- 684-redundant-connection/684-redundant-connection.py
+++ 684-redundant-connection/684-redundant-connection.py
@@ -2,20 +2,20 @@ class Solution:
     def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
         n = len(edges)
         p = list(range(n + 1))
+        print(p)
         
-        def find(u):
-            if p[u] != u:
-                p[u] = find(p[u])
-            return p[u]
+        def find(vertex):
+            if p[vertex] != vertex:
+                p[vertex] = find(p[vertex])
+            return p[vertex]
         
-        def union(a, b):
-            p[find(a)] = find(b)
+        def union(v1, v2):
+            p[find(v1)] = find(v2)
             
-            
-        for node1, node2 in edges:
-            if find(node1) != find(node2):
-                union(node1, node2)
+        
+        for v1, v2 in edges:
+            if find(v1) != find(v2):
+                union(v1, v2)
             else:
-                return [node1, node2]
-        
+                return [v1, v2]
         return []
