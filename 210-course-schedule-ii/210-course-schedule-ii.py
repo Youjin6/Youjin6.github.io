b93a329d7a23d982a00d4f8f44ae1046e619c724
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-09-23 20:34:47 -0700

    Time: 249 ms (16.30%), Space: 15.5 MB (71.42%) - LeetHub

--- 210-course-schedule-ii/210-course-schedule-ii.py
+++ 210-course-schedule-ii/210-course-schedule-ii.py
@@ -1,25 +1,26 @@
 class Solution:
-    def findOrder(self, n: int, prerequisites: List[List[int]]) -> List[int]:
-    
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         dic = defaultdict(list)
-        ind = [0] * n
+        indegree = [0] * numCourses
+        queue = deque([])
+        ans = []
         
-        for [course, pre] in prerequisites:
-            dic[pre].append(course)
-            ind[course] += 1
-        
-        q = deque([i for i in range(n) if ind[i] == 0])
-        
-        res = []
-        while q:
-            front = q.popleft()
-            res.append(front)
-            for i in dic[front]:
-                ind[i] -= 1
-                if ind[i] == 0:
-                    q.append(i)
-        
-        if len(res) == n:
-            return res
-        return []
+        for course, pre in prerequisites:
+            dic[pre].append(course) 
+            indegree[course] += 1
             
+        # append the course indegree is 0 to queue
+        for i in range(numCourses):
+            if indegree[i] == 0:
+                queue.append(i)
+        
+
+        while queue:
+            front = queue.popleft()
+            ans.append(front)
+            for course in dic[front]:
+                indegree[course] -= 1
+                if indegree[course] == 0:
+                    queue.append(course)
+                    
+        return ans if len(ans) == numCourses else []
