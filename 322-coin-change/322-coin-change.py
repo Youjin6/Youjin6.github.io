f47e7816baae42e3da1b0492fc48d5135b730ec1
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-09-25 10:31:42 -0700

    Time: 6457 ms (5.00%), Space: 16 MB (26.24%) - LeetHub

--- 322-coin-change/322-coin-change.py
+++ 322-coin-change/322-coin-change.py
@@ -1,15 +1,16 @@
 class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         n = len(coins)
-        f = [1e8] *(amount + 1)
+        f = [[1e8] * (amount + 1) for _ in range(n + 1)]
         
-        f[0] = 0
+        # base case
+        f[0][0] = 0
         
-        for i in coins:
-            for j in range(i, amount + 1):
-                f[j] = min(f[j], f[j - i] + 1)
-                    
-        if f[amount] == 1e8:
-            return -1
-        return f[amount]
-    
+        for i in range(1, n + 1):
+            for j in range(amount + 1):
+                f[i][j] = f[i - 1][j]
+                if j >= coins[i - 1]:
+                    f[i][j] = min(f[i][j], f[i][j - coins[i - 1]] + 1)
+        
+        res = f[n][amount]
+        return -1 if res == 1e8 else res
