f10442ed2052fb012dff594aafa6e150fa93f179
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-09-25 12:48:39 -0700

    Time: 1289 ms (18.82%), Space: 15.1 MB (81.41%) - LeetHub

--- 55-jump-game/55-jump-game.py
+++ 55-jump-game/55-jump-game.py
@@ -4,6 +4,16 @@ class Solution:
         for i in range(len(nums)):
             if farthest < i:
                 return False
-            farthest = max(farthest, nums[i] + i)
+            farthest = max(farthest, i + nums[i])
+            
         
         return True
+    
+    
+    
+"""
+[2, 3, 1, 1, 4]
+i = 0, 1,
+farthest position: 2 -> 4. farthest = max(farthest, i + nums[i] )
+
+"""
