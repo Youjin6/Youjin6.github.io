3341b3d0597547b324eb6c7dcc3b9e7479bc7712
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-09-25 11:08:20 -0700

    Time: 1070 ms (27.88%), Space: 29.5 MB (41.98%) - LeetHub

---
+++ 518-coin-change-ii/518-coin-change-ii.py
@@ -0,0 +1,14 @@
+class Solution:
+    def change(self, amount: int, coins: List[int]) -> int:
+        n = len(coins)
+        
+        f = [[0] * (amount + 1) for _ in range(n + 1)]
+        f[0][0] = 1
+        ways = 0
+        for i in range(1, n + 1):
+            for j in range(amount + 1):
+                f[i][j] = f[i - 1][j]
+                if j >= coins[i - 1]:
+                    f[i][j] += f[i][j - coins[i - 1]]
+        
+        return f[n][amount]
