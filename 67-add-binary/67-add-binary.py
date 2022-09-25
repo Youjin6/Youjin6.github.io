4e169b35900db111d8318a039f2e24da9a19b0a4
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-09-25 16:34:31 -0700

    Time: 41 ms (83.24%), Space: 13.9 MB (25.66%) - LeetHub

--- 67-add-binary/67-add-binary.py
+++ 67-add-binary/67-add-binary.py
@@ -1,20 +1,20 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        n = len(a) - 1
-        m = len(b) - 1
-        carry = 0
-        res = ''
+        n , m = len(a) - 1, len(b) - 1
         
+        carry = 0
+        ans = ''
         while n >= 0 or m >= 0:
             if n >= 0:
-                carry += ord(a[n]) - ord('0')
+                carry += int(a[n])
                 n -= 1
             if m >= 0:
-                carry += ord(b[m]) - ord('0')
+                carry += int(b[m])
                 m -= 1
-            res = str(carry % 2) + res
+            
+            ans = str(carry % 2) + ans
             carry //= 2
         
         if carry:
-            res = '1' + res
-        return res
+            ans = '1' + ans
+        return ans
