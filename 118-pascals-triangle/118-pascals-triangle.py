7988f6ebf644a7a6f31601937c6c6b4ab0257cba
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-10-08 17:17:08 -0700

    Time: 62 ms (24.24%), Space: 13.9 MB (65.92%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.py
+++ 118-pascals-triangle/118-pascals-triangle.py
@@ -1,12 +1,15 @@
 class Solution:
-    def generate(self, numRows: int) -> List[List[int]]:
-        res = [[1]]
-        for i in range(numRows - 1):
-            helper = [0] + res[-1] + [0]
-            cur_row = []
-            for j in range(len(res[-1]) + 1):
-                cur_row.append(helper[j] + helper[j + 1])
+    def generate(self, n: int) -> List[List[int]]:
+        res = []
+        for i in range(0, n):
+            cur_row = [0] * (i + 1)
+            cur_row[0] = cur_row[i] = 1
+            for j in range(1, i):
+                cur_row[j] = res[i - 1][j - 1] + res[i - 1][j]
             
             res.append(cur_row)
+            
         
         return res
+            
+        
