9d177f4f6e195635df24b88821defe9a241c126b
Youjin6 <70083559+Youjin6@users.noreply.github.com>
2022-10-31 22:38:21 -0700

    Time: 9254 ms (5.02%), Space: 13.8 MB (97.33%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -11,7 +11,7 @@ class Solution:
         
         for i in nums:
             for j in reversed(range(i, s + 1)):
-                f[j] = f[j] or f[j - i]
+                f[j] = max(f[j], f[j - i])
         
         
         return f[s]
